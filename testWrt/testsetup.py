0c9a43893d2a5fab1bbf4d32c2e77b999d883711
Alexander Couzens <lynxis@fe80.eu>
2014-03-13 17:38:29 +0100

    testsetup: openwrt() returns openwrt testobject + renamed functions

--- testWrt/testsetup.py
+++ testWrt/testsetup.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python
 
-from .wifinodes import *
+import logging
+
+from testWrt.lib import OpenWrtBase
+
+LOG = logging.getLogger('testWrt')
 
 class TestSetup(object):
     def __init__(self):
         # type: []
         self._devices = {}
+        self._openwrt = None
 
-    def addDevice(self, device):
+    def add_device(self, device):
         if device.name in self._devices:
             self._devices[device.name].append(device)
         else:
             self._devices[device.name] = [device]
+
+    def set_openwrt(self, openwrt):
+        """ set your main test subject """
+        if not isinstance(openwrt, OpenWrtBase):
+            raise RuntimeError("Wrong type! Need an OpenWrtBase object)")
+        if self._openwrt:
+            LOG.warn("Already set an openwrt device")
+        self._openwrt = openwrt
+
+    @property
+    def openwrt(self):
+        """
+        return an OpenWrt object (derived from OpenWrtBase)
+
+        @rtype OpenWrtBase
+        """
+        return self._openwrt
 
 def create_generic():
     tsetup = TestSetup()
