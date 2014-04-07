025504431de3e821648908fe7bf1509434e94ada
Alexander Couzens <lynxis@fe80.eu>
2014-04-07 23:13:29 +0200

    basic uci - fixme up

--- tests/test_uci.py
+++ tests/test_uci.py
@@ -2,6 +2,8 @@
 from config import TestSetup
 import logging
 
+from testWrt.lib.testing import assert_stdoutlines, assert_stderrlines
+
 LOG = logging.getLogger("test")
 
 class TestOpenWrt(object):
@@ -10,20 +12,14 @@ class TestOpenWrt(object):
         self.owrt = TestSetup.openwrt
 
     def test_uci_import(self):
-        stdin, stdout, stderr = self.owrt.execute("uci import tests")
+        streams = self.owrt.execute("uci import tests")
 
-        stdin.write("config tsss\n")
-        stdin.write("    option foo bar\n")
+        streams[0].write("config testconfig\n")
+        streams[0].write("    option testoption testvalue\n")
         # shutdown in and output of stdin to send an EOF
-        stdin.channel.shutdown(2)
-        outlines = stdout.readlines()
-        errlines = stderr.readlines()
-        LOG.debug("StdErr lines")
-        LOG.debug(errlines)
-        LOG.debug("StdOut lines")
-        LOG.debug(outlines)
-        assert len(errlines) == 0
-        assert len(outlines) == 0
+        streams[0].channel.shutdown(2)
+        assert_stderrlines(streams)
+        assert_stdoutlines(streams)
 
     def test_uci_export(self):
         stdin, stdout, stderr = self.owrt.execute("uci export tests")
@@ -34,7 +30,18 @@ class TestOpenWrt(object):
         LOG.debug("StdOut lines")
         LOG.debug(outlines)
         assert outlines[0] == "package tests\n"
-        assert outlines[2] == "config tsss\n"
-        assert outlines[3] == "\toption foo 'bar'\n"
+        assert outlines[2] == "config testconfig\n"
+        assert outlines[3] == "\toption testoption 'testvalue'\n"
         assert len(errlines) == 0
 
+    def test_uci_add(self):
+        streams = self.owrt.execute_one_shot('uci add tests uciadd')
+        assert_stderrlines(streams)
+
+    def test_uci_set(self):
+        streams = self.owrt.execute_one_shot('uci set tests.uciadd.testoption=foo')
+        assert_stderrlines(streams)
+
+    def test_uci_get(self):
+        streams = self.owrt.execute_one_shot('uci get tests.uciadd')
+        assert_stderrlines(streams)
